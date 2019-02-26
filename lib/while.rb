777b0da7b78cfbe34baf6376a96227c426af6283
Okunola Jelilat Dasola <jelilatokunola2809@gmail.com>
2019-02-26 23:06:30 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
   levitation_force = 6
 	while levitation_force < 10
-puts "Wingardium leviosa"  #your code here
+puts "Wingardium Leviosa"  #your code here
 levitation_force +=1
 end
 
